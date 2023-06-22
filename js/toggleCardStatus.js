b43e9f484187b970bd663798ab9820ee38ac4033
Scorpion96522 <126128140+Scorpion96522@users.noreply.github.com>
2023-06-22 18:57:39 +0300

    added the checkout function

--- js/toggleCardStatus.js
+++ js/toggleCardStatus.js
@@ -4,11 +4,16 @@ function toggleCardStatus() {
     const cardWrapper = document.querySelector('.cart-wrapper');
     
     const cardEmptyBadge = document.querySelector('[data-cart-empty]');
+
+    const orderForm = document.querySelector('[#order-form]');
+   
     if (cardWrapper.children.length > 0) {
         console.log('full');
         cardEmptyBadge.classList.add('none');
+        orderForm.classList.remove('none');
     } else {
         console.log('epty');
         cardEmptyBadge.classList.remove('none');
+        orderForm.classList.add('none');
     }
 }
